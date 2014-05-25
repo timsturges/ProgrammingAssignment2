2dd3838f0ad5e119deef1067a8fb2cf5469a0742
timsturges <timsturges@gmail.com>
2014-05-25 06:32:02 -0400

    -allow-empty

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a submission for the second programming assignment for rprog-003
+## These functions exploit scoping rules to preserve object states
+## These functions are modified from example code provided in the assignment
 
-## Write a short comment describing this function
+## This function creates a list of functions which create and return a matrix
+## and to create and return the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function () x
+        setinv <- function(mtrxinvrs) inv <<- mtrxinvrs
+        getinv <- function() inv
+        list(set = set,get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function actually solves the matrix inverse, but first checks the 
+## matrix list from makeCacheMatrix to see if the value has been solved already
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data,...)
+        x$setinv(inv)
+        inv
 }
